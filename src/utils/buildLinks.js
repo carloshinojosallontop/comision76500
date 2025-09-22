ab28f4646da2a7fe648dbb57778cbb202ae8d3d0
carlos hinojosa <fabianhll@hotmail.com>
2025-09-22 13:59:43 -0500

    utils configuradas

--- src/utils/buildLinks.js
+++ src/utils/buildLinks.js
@@ -1,4 +1,11 @@
-export function buildPageLinks(req, page, hasPrev, prevPage, hasNext, nextPage) {
+export function buildPageLinks(
+  req,
+  page,
+  hasPrev,
+  prevPage,
+  hasNext,
+  nextPage
+) {
   const base = `${req.protocol}://${req.get("host")}${req.baseUrl}${req.path}`;
   const qp = new URLSearchParams({ ...req.query });
   const link = (p) => {
